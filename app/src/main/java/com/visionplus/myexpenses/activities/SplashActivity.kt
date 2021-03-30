6aa535e5590f33221f90be50291737fb6a63be9e
PC03\PC <mseTrainer91@gmail.com>
2021-03-30 17:35:42 +0200

    Splash screen with animation

---
+++ app/src/main/java/com/visionplus/myexpenses/activities/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.visionplus.myexpenses.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.visionplus.myexpenses.R
+import com.visionplus.myexpenses.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val animation = AnimationUtils.loadAnimation(this,R.anim.bounce)
+        animation.setAnimationListener(object:Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {}
+
+            override fun onAnimationEnd(p0: Animation?) {
+               val intent = Intent(this@SplashActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+        binding.imgLogo.startAnimation(animation)
+    }
+}
